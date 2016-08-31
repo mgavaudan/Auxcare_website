6d6b3cac9de9d76826890a996da08fa3f36d4a72
Matthieu Gavaudan <mlg2199@columbia.edu>
2016-08-31 03:48:39 +0100

    final design of home + started dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,16 @@
 import React, { Component, PropTypes } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+import Badge from 'material-ui/Badge';
+import IconButton from 'material-ui/IconButton';
+import NotificationsIcon from 'material-ui/svg-icons/social/notifications';
 import FlatButton from 'material-ui/FlatButton';
-import {cyan500} from 'material-ui/styles/colors';
+
+
+import {fullWhite, lightBlue500} from 'material-ui/styles/colors';
+
+import '../styles/dashboard.scss';
+
 
 export default class Dashboard extends Component {
 
@@ -11,21 +20,45 @@ export default class Dashboard extends Component {
 		return (
 			<MuiThemeProvider>
 			<div>
-				<FlatButton label='Logout' backgroundColor={cyan500} onClick={() => onLogoutClick()} />
-				<FlatButton label='Secret Quote' backgroundColor={cyan500} onClick={() => onSecretQuoteClick('protected/doctor/doctor1@gmail.com', true)} />
-
-				<div >
-				{data &&
+				<div className='logo'>
+					Auxcare
+				</div>
+				<div className='menu'>
+				</div>
+				<div className='topmenu'>
+					<Badge style={styles.badge} badgeContent={10} secondary={true} badgeStyle={{top: 12, right: 12}}>
+						<IconButton tooltip='Notifications'>
+						<NotificationsIcon color={fullWhite}/>
+						</IconButton>
+					</Badge>
+					<FlatButton style={styles.logout} label='Logout' backgroundColor={lightBlue500} onClick={() => onLogoutClick()} />
+				</div>
+				<div className='main'>
+					<FlatButton label='Secret Quote' backgroundColor={lightBlue500} onClick={() => onSecretQuoteClick('protected/doctor/doctor1@gmail.com', true)} />
+					{data &&
 					<div>
 						<blockquote>{data.quotes}</blockquote>
 					</div>
-				}
+					}
 				</div>
 			</div>
 			</MuiThemeProvider>
 		);
 	}
 }
+
+
+const styles = {
+	badge: {
+		position: 'absolute',
+		top: 0,
+	},
+	logout: {
+		position: 'absolute',
+		right: 15,
+		top: 17,
+	},
+};
 
 Dashboard.propTypes = {
 	onLogoutClick: PropTypes.func.isRequired,
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,9 +1,24 @@
 import React, { Component, PropTypes } from 'react';
 // import { Link } from 'react-router';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+import RaisedButton from 'material-ui/RaisedButton';
 import FlatButton from 'material-ui/FlatButton';
-import {cyan500} from 'material-ui/styles/colors';
 import TextField from 'material-ui/TextField';
+import {Tabs, Tab} from 'material-ui/Tabs';
+
+import {fullWhite, lightBlue500, cyan200} from 'material-ui/styles/colors';
+import ActionGetApp from 'material-ui/svg-icons/action/get-app';
+import ActionImportantDevices from 'material-ui/svg-icons/action/important-devices';
+import ActionAccountBox from 'material-ui/svg-icons/action/account-box';
+import ActionAssessment from 'material-ui/svg-icons/action/assessment';
+import ActionAssignment from 'material-ui/svg-icons/action/assignment';
+import CommunicationLiveHelp from 'material-ui/svg-icons/communication/live-help';
+import CommunicationEmail from 'material-ui/svg-icons/communication/email';
+import NavigationArrowForward from 'material-ui/svg-icons/navigation/arrow-forward';
+
+
 import '../styles/styles.scss';
 
 
@@ -27,20 +42,60 @@ export default class LoginPage extends Component {
 		const { errorMessage } = this.props;
 
 		return (
-			<MuiThemeProvider>	
-			<div style={styles.main}>
-				<h1>React Hack</h1>
-				<h2>Login</h2>
-
-				<TextField type='text' ref='email' hintText='Email'/>
-				<br/><br/>
-				<TextField type='password' ref='password' hintText='Password'/>
-				<br/><br/>
-				<FlatButton label='Login' backgroundColor={cyan500} onClick={() => this.handleClick()}/>
-
-				{errorMessage &&
-					<p>{errorMessage}</p>
-				}
+			<MuiThemeProvider muiTheme={muiTheme}>	
+			<div className='parent'>
+				<img src={require('../../images/logo.png')}/>
+				<div className='nav'>
+					<FlatButton hoverColor={lightBlue500} label='Who are we?' onClick={() => console.log('we are')}/>
+					<FlatButton hoverColor={lightBlue500} label='Our Data' onClick={() => console.log('allo')}/>	
+					<FlatButton hoverColor={lightBlue500} label='Learn More' onClick={() => console.log('noone')}/>
+					<FlatButton hoverColor={lightBlue500} label='Contact Us' onClick={() => console.log('noone')}/>
+				</div>
+				<div style={styles.main}>
+					<div className='login'>
+						<h3>Login</h3>
+						<TextField type='text' ref='email' hintText='Email' color={fullWhite}/>
+						<br/><br/>
+						<TextField type='password' ref='password' hintText='Password'/>
+						<br/><br/>
+						<RaisedButton style={styles.but} backgroundColor={lightBlue500} label='Login' onClick={() => this.handleClick()}/>
+						{errorMessage &&
+							<p>{errorMessage}</p>
+						}
+					</div>
+					<div className='getstarted'>
+						<Tabs inkBarStyle={styles.ink}>
+						<Tab style={styles.tab} label='Step One' >
+							<div className='tab'>
+								<p> To get started with Auxcare : </p>
+								
+								<p> <CommunicationLiveHelp style={styles.icon}/> Learn about our platform.</p>
+								<p> 
+								<ActionAccountBox/>
+								<NavigationArrowForward/>
+								<CommunicationEmail/>
+								<br/>
+								Sign Up and we'll email you!
+								</p>
+							</div>
+						</Tab>
+						<Tab style={styles.tab} label='Step Two' >
+							<div className='tab'>
+								<p> <ActionGetApp style={styles.icon}/> Ask your patients to download our smartphone app. </p>
+								<p> <ActionAssessment style={styles.icon}/> They will take weekly medically accurate tests. </p>
+							</div>
+						</Tab>
+						<Tab style={styles.tab} label='Step Three'>
+							<div className='tab'>
+								<p> <ActionImportantDevices style={styles.icon}/> Get the results right here on this website! </p>
+								<p> <ActionAssignment style={styles.icon}/> Check on their symptom progression and tailor your treatment! </p>
+							</div>
+						</Tab>
+						</Tabs>
+						<br/>
+						<RaisedButton style={styles.but} backgroundColor={lightBlue500} label='Get Started!' onClick={() => this.handleClick()}/>
+					</div>
+				</div>
 			</div>
 			</MuiThemeProvider>
 		);
@@ -49,18 +104,40 @@ export default class LoginPage extends Component {
 }
 
 const styles = {
-  main: {
-    textAlign: 'center',
-    paddingTop: 200,
-    position: 'absolute',
-    top:0,
-    bottom:0,
-    right:0,
-    left:0,
-  },
+	tab: {
+		backgroundColor: lightBlue500,
+		height: 50,
+	},
+	icon: {
+		marginRight: 190,
+		marginLeft: 190,
+		lineHeight: 1,
+	},	
+	ink: {
+		backgroundColor: cyan200,
+	},
+	but: {
+		position: 'absolute',
+		right: 100,
+		left:100,
+		bottom: 25,
+	},
+	main: {
+		paddingTop: 150,
+		paddingLeft: 195,
+		position: 'absolute',
+		top:50,
+		bottom:0,
+		right:0,
+		left:0,
+	},
 };
 
-
+const muiTheme = getMuiTheme({
+  palette: {
+    textColor: fullWhite,
+  },
+});
 
 LoginPage.propTypes = {
 	onLoginClick: PropTypes.func.isRequired,
